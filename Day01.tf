9667f946c792d87e13ca885b93e177490931c606
Sushovan Medda <62806449+sushovanmedda98@users.noreply.github.com>
2024-04-02 03:44:02 +0530

    Update Day01.tf

--- Day01.tf
+++ Day01.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
 
+resource "aws_instance" "terraform" {
+    ami = "ami-0c101f26f147fa7fd"
+    instance_type = "t2.micro"
+    key_name = "*****"
+    subnet_id = "subnet-06a41824b8d0bc464"
+  
+}
