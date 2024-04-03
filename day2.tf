271bce694e2bc027414eb41877d4d6a5ebbdf338
sushovanmedda98 <sushovanmedda98@gmail.com>
2024-04-03 06:02:31 +0530

    new

---
+++ day2.tf
@@ -0,0 +1,33 @@
+variable "ami_value" {
+    description = "value for ami"
+  
+}
+
+variable "instance_type_value" {
+    description = "value for instance"
+  
+}
+
+variable "subnet_id_value" {
+    description = "value for subnet"
+  
+}
+variable "key_name_value" {
+    description = "value for key"
+  
+}
+
+
+
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+resource "aws_instance" "example" {
+    ami = "var.ami_value"
+    instance_type = "var.instance_type_value"
+    subnet_id = "var.subnet_id_value"
+    key_name = "var.key_name_value"
+  
+}
---
+++ terraform2.tfvars
@@ -0,0 +1,4 @@
+ami_value="ami-1525gsghdhhdh"
+instance_type_value="t2.micro"
+subnet_id_value="10/0/0/0"
+key_name_value="bitun"
