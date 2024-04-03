06676239a056b65e89874545dffed453f7c87ef9
sushovanmedda98 <sushovanmedda98@gmail.com>
2024-04-04 02:51:27 +0530

    variable file added

---
+++ variables2.tf
@@ -0,0 +1,18 @@
+variable "ami_value" {
+    description = "value for ami"
+  
+}
+
+variable "instance_type_value" {
+    description = "value for instance"
+  
+}
+
+variable "subnet_id_value" {
+    description = "value for subnet"
+  
+}
+variable "key_name_value" {
+    description = "value for key"
+  
+}
